d7e4763546e02b188e48047cfcee936b57b899a9
tanakaedu <tanaka@am1.jp>
2019-11-29 16:47:45 +0900

    ステラのマテリアルとテクスチャーを設定

--- Assets/Scripts/SceneManagers/GameManager.cs
+++ Assets/Scripts/SceneManagers/GameManager.cs
@@ -6,19 +6,23 @@ namespace GreeningEx2019
 {
     public class GameManager : SceneManagerBase
     {
+        [Tooltip("デバッグキーを有効にする"), SerializeField]
+        bool useDebugKey = false;
 
         private void Update()
         {
 #if DEBUG
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                // エンディングへ
-                SceneChanger.ChangeScene(SceneChanger.SceneType.StageSelect);
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                // エンディングへ
-                SceneChanger.ChangeScene(SceneChanger.SceneType.Ending);
+            if (useDebugKey) {
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    // エンディングへ
+                    SceneChanger.ChangeScene(SceneChanger.SceneType.StageSelect);
+                }
+                else if (Input.GetKeyDown(KeyCode.E))
+                {
+                    // エンディングへ
+                    SceneChanger.ChangeScene(SceneChanger.SceneType.Ending);
+                }
             }
 #endif
         }
