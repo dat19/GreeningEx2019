7d1aaac00317b2e36f8219693f1b578c978ffe5c
tanakaedu <tanaka@am1.jp>
2019-12-06 16:55:33 +0900

    仮の水まき実装

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    [Tooltip("生存秒数"), SerializeField]
+    float lifeTime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);        
+    }
+}
