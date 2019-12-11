97ccc9a2861bfdffd9bd0ca04fc0260626f309ab
tanakaedu <tanaka@am1.jp>
2019-12-11 15:29:03 +0900

    ツタとキノコの成長を実装

---
+++ Assets/Scripts/AnimEvents.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// アニメーションイベントを受け取って、
+/// 設定されているイベントを実行します。
+/// </summary>
+public class AnimEvents : MonoBehaviour
+{
+    [Tooltip("呼び出した時に実行するイベント"), SerializeField]
+    UnityEvent onCallFromAnimation;
+
+    public void Proc()
+    {
+        onCallFromAnimation?.Invoke();
+    }
+}
---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [Tooltip("プレイヤーの画面左端"), SerializeField]
+    float viewPointMin = 0.2f;
+    [Tooltip("プレイヤーの画面右端"), SerializeField]
+    float viewPointMax = 0.6f;
+
+    Camera myCamera;
+    Transform playerTransform;
+    Vector3 camToPlayer;
+
+    private void Awake()
+    {
+        myCamera = GetComponent<Camera>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        camToPlayer = playerTransform.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 next = playerTransform.position - camToPlayer;
+        transform.position = next;
+    }
+}
