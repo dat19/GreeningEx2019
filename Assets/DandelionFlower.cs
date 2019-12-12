83fbd9d96450eba30e2ac4c45fb278b3280f26f2
tanakaedu <tanaka@am1.jp>
2019-12-12 16:11:51 +0900

    たんぽぽ追加

---
+++ Assets/DandelionFlower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DandelionFlower : MonoBehaviour
+{
+    Animator animator = null;
+    int state = 0;
+    [SerializeField]
+    Vector3 fluffOffset = new Vector3(0f, 0.5f, 0);
+    [SerializeField]
+    GameObject Fluff = null;
+    [SerializeField]
+    float insTime = 2;
+    float lastTime;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water") || state == 0)
+        {
+            animator.SetTrigger("bloom");
+            state = 1;
+        }
+    }
+    public void ToFluff()
+    {
+        state = 2;
+    }
+    private void FixedUpdate()
+    {
+        if (state == 2)
+        {
+            if (Time.time - lastTime > insTime)
+            {
+                Instantiate(Fluff, transform.position + fluffOffset, Quaternion.identity);
+                lastTime = Time.time;
+            }
+        }
+    }
+}
