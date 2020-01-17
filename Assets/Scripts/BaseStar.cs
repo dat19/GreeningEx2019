4179fe157135275357b4f7ee4bdcf6ff9ce092cd
tanakaedu <tanaka@am1.jp>
2020-01-17 19:43:15 +0900

    データのバイナリ生成開始

--- Assets/Scripts/BaseStar.cs
+++ Assets/Scripts/BaseStar.cs
@@ -3,6 +3,7 @@
 
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace GreeningEx2019
@@ -38,11 +39,9 @@ namespace GreeningEx2019
         /// <summary>
         /// 海用テクスチャーの大きさ
         /// </summary>
-        const int SeaTextureSize = 128;
-        /// <summary>
-        /// 島からの影響距離
-        /// </summary>
-        const float IslandDistance = 4f / 128f;
+        public const int SeaTextureSize = 128;
+
+        public const string SeaTextureRatesFileName = "SeaTextureRates.bin";
 
         /// <summary>
         /// 実際に海に貼り付けるテクスチャー
@@ -54,11 +53,20 @@ namespace GreeningEx2019
         Color32[] dirtyColors;
         Color32[] cleanColors;
 
+        byte[] seaTextureRates = null;
+
         private void Start()
         {
             // 実際に貼り付けるテクスチャーの作成
             seaTexture = new Texture2D(SeaTextureSize, SeaTextureSize, dirtySeaTexture.format, false);
             seaColors = seaTexture.GetPixels32();
+
+            TextAsset asset = Resources.Load("SeaTextureRatesFileName") as TextAsset;
+            Stream s = new MemoryStream(asset.bytes);
+            BinaryReader br = new BinaryReader(s);
+            byte[] rates = br.ReadBytes((int)s.Length);
+
+            Debug.Log($"---- readed {rates.Length}");
 
             /*
             // GetUVのチェック
@@ -80,16 +88,16 @@ namespace GreeningEx2019
             }
             */
 
-            // 1面クリア状態で試す
-            float [] beforeRates = CalcCleanRate(2);
-
             dirtyColors = dirtySeaTexture.GetPixels32();
             cleanColors = cleanSeaTexture.GetPixels32();
             dirtyColors[0].a = 0;
 
             for (int i = 0; i < seaColors.Length; i++)
             {
-                seaColors[i] = Color32.Lerp(dirtyColors[0], cleanColors[0], Mathf.Clamp01(beforeRates[i]));
+                seaColors[i] = Color32.Lerp(
+                    dirtyColors[0],
+                    cleanColors[0],
+                    Mathf.Clamp01(((float)rates[i]))/256f);
             }
             seaTexture.SetPixels32(seaColors);
             seaTexture.Apply();
@@ -147,126 +155,6 @@ namespace GreeningEx2019
             Vector3 axis = Vector3.Cross(stageDir, Vector3.back);
             float angle = Vector3.SignedAngle(stageDir, Vector3.back, axis);
             transform.RotateAround(transform.position, axis, angle * rotateRate);
-        }
-
-        /// <summary>
-        /// クリアしたステージの数(0～GameParams.StageMax)を受け取って、
-        /// そのクリア状態に対応した海のテクスチャの割合の配列を返します。
-        /// </summary>
-        /// <param name="clearedCount">0～GameParams.StageMax</param>
-        /// <returns>算出したクリア前とクリア後のブレンド値(0～1)</returns>
-        float[] CalcCleanRate(int clearedCount)
-        {
-            float[] rates = new float[SeaTextureSize * SeaTextureSize];
-            Vector2[][] uvs = new Vector2[GameParams.StageMax][];
-
-            // 全ての島の頂点をすべて算出して、UV値に変換する
-            for (int i=0;i<islands.Length;i++)
-            {
-                MeshFilter mf = meshIslands[i].GetComponent<MeshFilter>();
-                uvs[i] = new Vector2[mf.mesh.vertexCount];
-                for (int j=0;j<mf.mesh.vertexCount;j++)
-                {
-                    Vector3 pos = meshIslands[i].transform.TransformPoint(mf.mesh.vertices[j]);
-                    // UV値を求める
-                    uvs[i][j] = GetUV(pos);
-#if DEBUG_SPHERE
-                    if (i == 0)
-                    {
-                        Instantiate(debugSphere, pos, Quaternion.identity, transform);
-                        Log($"  [{j}] pos={pos.x}, {pos.y}, {pos.z} / uv={uvs[i][j].x}, {uvs[i][j].y}");
-                    }
-#endif
-                }
-            }
-
-            // 各頂点の影響値を算出する
-            int idx = 0;
-            Vector2 check = Vector2.zero;
-            for (int y=0;y<SeaTextureSize;y++)
-            {
-                for (int x=0;x<SeaTextureSize;x++, idx++)
-                {
-                    rates[idx] = 0;
-                    check.Set((float)x / (float)SeaTextureSize, (float)y / (float)SeaTextureSize);
-                    for (int i=0;i<GameParams.StageMax;i++)
-                    {
-                        float clearScore = i < clearedCount ? 1 : -1;
-
-                        // 各島の頂点ごとのUV値
-                        for (int vi=0;vi<uvs[i].Length;vi++)
-                        {
-                            Vector2 mag = check - uvs[i][vi];
-                            // 0.5を超えたらループ
-                            if (mag.x < -0.5f) mag.x += 1f;
-                            if (mag.x > 0.5f) mag.x -= 1f;
-                            if (mag.y < -0.5f) mag.y += 1f;
-                            if (mag.y > 0.5f) mag.y -= 1f;
-                            // 影響範囲チェック
-                            if (mag.magnitude < IslandDistance)
-                            {
-                                float rate = 1f-(mag.magnitude / IslandDistance);
-                                rates[idx] += clearScore * rate;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return rates;
-        }
-
-        /// <summary>
-        /// 指定の方向
-        /// </summary>
-        /// <param name="dir">中心からの方向</param>
-        /// <returns>uv値。0～1で正規化した値を返します。</returns>
-        public static Vector2 GetUV(Vector3 dir)
-        {
-            Vector2 uv = Vector2.zero;
-
-            // 角度を測るときの中心のベクトル
-            Vector3 baseDir = new Vector3(dir.x, 0f, dir.z);
-            if (baseDir.magnitude < 0.0001f)
-            {
-                // 真上
-                if (dir.y > 0f)
-                {
-                    return Vector2.zero;
-                }
-                // 真下
-                return Vector2.one*0.999f;
-            }
-
-            baseDir.Normalize();
-
-            // u値は、右ベクトルとのなす角
-            float angle = Vector3.SignedAngle(Vector3.forward, baseDir, Vector3.up);
-            uv.x = -angle / 360f + (11f / 16f);
-            if (uv.x > 1f)
-            {
-                uv.x -= 1f;
-            }
-            Log($"  angle={angle} / uv.x={uv.x}");
-
-            // v値は、baseDirと、dirのなす角
-            float dot = Vector3.Dot(baseDir, dir.normalized);
-            if (Mathf.Approximately(dot, 1f))
-            {
-                // ほぼ1の時はベクトル一致なので、なす角は0
-                Log($"  zero");
-                uv.y = 0.5f;
-            }
-            else
-            {
-                angle = Vector3.Angle(baseDir, dir);
-                if (dir.y > 0f) { angle = -angle; }
-                Log($"  angle y={angle}");
-                uv.y = (-angle+90f) / 180f;
-                Log($"  baseDir={baseDir} / dir={dir} / uv.y={uv.y}");
-            }
-
-            return uv;
         }
 
         [System.Diagnostics.Conditional("DEBUG_LOG")]
--- Assets/Scripts/Dev/MakeSeaRange.cs
+++ Assets/Scripts/Dev/MakeSeaRange.cs
@@ -13,21 +13,139 @@ namespace GreeningEx2019
 
         [Tooltip("データ生成を実行"), SerializeField]
         bool isRun = false;
-        [Tooltip("海テクスチャーの一辺のサイズ"), SerializeField]
-        int seaSize = 128;
         [Tooltip("頂点からの有効範囲。正規座標系"), SerializeField]
         float islandDistance = 4f / 128f;
 
         void Start()
         {
-            byte[] writeData = new byte[seaSize * seaSize];
-
-            for (int i=0; i<256;i++)
-            {
-                writeData[i] = (byte)i;
-                Debug.Log($"{writeData[i]}");
-            }
+            // 1面クリア状態で試す
+            byte[] beforeRates = CalcCleanRate(0);
+            File.WriteAllBytes($"Output/{BaseStar.SeaTextureRatesFileName}", beforeRates);
         }
+
+        /// <summary>
+        /// 指定の星から各テクスチャが受ける影響の値を算出して、0～255で返します。
+        /// </summary>
+        /// <param name="取得したいステージ番号">0～GameParams.StageMax</param>
+        /// <returns>算出した影響(0～255)</returns>
+        byte[] CalcCleanRate(int stg)
+        {
+            byte[] rates = new byte[BaseStar.SeaTextureSize * BaseStar.SeaTextureSize];
+
+            MeshFilter mf = meshIslands[stg].GetComponent<MeshFilter>();
+            Vector2[] uvs = new Vector2[mf.mesh.vertexCount];
+            for (int i = 0; i < mf.mesh.vertexCount; i++)
+            {
+                Vector3 pos = meshIslands[stg].transform.TransformPoint(mf.mesh.vertices[i]);
+                // UV値を求める
+                uvs[i] = GetUV(pos);
+#if DEBUG_SPHERE
+                if (i == 0)
+                {
+                    Instantiate(debugSphere, pos, Quaternion.identity, transform);
+                    Log($"  [{j}] pos={pos.x}, {pos.y}, {pos.z} / uv={uvs[i][j].x}, {uvs[i][j].y}");
+                }
+#endif
+            }
+
+            // 各頂点の影響値を算出する
+            int idx = 0;
+            Vector2 check = Vector2.zero;
+            for (int y = 0; y < BaseStar.SeaTextureSize; y++)
+            {
+                for (int x = 0; x < BaseStar.SeaTextureSize; x++, idx++)
+                {
+                    rates[idx] = 0;
+                    check.Set(
+                        (float)x / (float)BaseStar.SeaTextureSize,
+                         (float)y / (float)BaseStar.SeaTextureSize);
+
+                    // 各島の頂点ごとのUV値
+                    for (int vi = 0; vi < uvs.Length; vi++)
+                    {
+                        Vector2 mag = check - uvs[vi];
+                        // 0.5を超えたらループ
+                        if (mag.x < -0.5f) mag.x += 1f;
+                        if (mag.x > 0.5f) mag.x -= 1f;
+                        if (mag.y < -0.5f) mag.y += 1f;
+                        if (mag.y > 0.5f) mag.y -= 1f;
+                        // 影響範囲チェック
+                        if (mag.magnitude < islandDistance)
+                        {
+                            float rate = 1f - (mag.magnitude / islandDistance);
+                            int dt = (int)(rate * 256f);
+                            if (dt == 256)
+                            {
+                                dt = 255;
+                            }
+                            rates[idx] = (byte)dt;
+                        }
+                    }
+                }
+            }
+
+            return rates;
+        }
+
+        /// <summary>
+        /// 指定の方向
+        /// </summary>
+        /// <param name="dir">中心からの方向</param>
+        /// <returns>uv値。0～1で正規化した値を返します。</returns>
+        public static Vector2 GetUV(Vector3 dir)
+        {
+            Vector2 uv = Vector2.zero;
+
+            // 角度を測るときの中心のベクトル
+            Vector3 baseDir = new Vector3(dir.x, 0f, dir.z);
+            if (baseDir.magnitude < 0.0001f)
+            {
+                // 真上
+                if (dir.y > 0f)
+                {
+                    return Vector2.zero;
+                }
+                // 真下
+                return Vector2.one * 0.999f;
+            }
+
+            baseDir.Normalize();
+
+            // u値は、右ベクトルとのなす角
+            float angle = Vector3.SignedAngle(Vector3.forward, baseDir, Vector3.up);
+            uv.x = -angle / 360f + (11f / 16f);
+            if (uv.x > 1f)
+            {
+                uv.x -= 1f;
+            }
+            Log($"  angle={angle} / uv.x={uv.x}");
+
+            // v値は、baseDirと、dirのなす角
+            float dot = Vector3.Dot(baseDir, dir.normalized);
+            if (Mathf.Approximately(dot, 1f))
+            {
+                // ほぼ1の時はベクトル一致なので、なす角は0
+                Log($"  zero");
+                uv.y = 0.5f;
+            }
+            else
+            {
+                angle = Vector3.Angle(baseDir, dir);
+                if (dir.y > 0f) { angle = -angle; }
+                Log($"  angle y={angle}");
+                uv.y = (-angle + 90f) / 180f;
+                Log($"  baseDir={baseDir} / dir={dir} / uv.y={uv.y}");
+            }
+
+            return uv;
+        }
+
+        [System.Diagnostics.Conditional("DEBUG_LOG")]
+        static void Log(object mes)
+        {
+            Debug.Log(mes);
+        }
+
     }
 }
 #endif
