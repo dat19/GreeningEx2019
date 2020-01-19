c875d086e536227a456e74a81cafa5d162d0cc58
Yu Tanaka <YRK00337@nifty.com>
2020-01-20 01:08:08 +0900

    島の浄化準備

--- Assets/Scripts/BaseStar.cs
+++ Assets/Scripts/BaseStar.cs
@@ -1,4 +1,4 @@
-﻿#define DEBUG_SPHERE    // メッシュの位置をSphereで示すデバッグ
+﻿//#define DEBUG_SPHERE    // メッシュの位置をSphereで示すデバッグ
 //#define DEBUG_LOG
 //#define DEBUG_CALC_SPHERE_POS   // 球体の座標の算出コードのテスト
 
@@ -20,10 +20,14 @@ namespace GreeningEx2019
         GameObject starPrefab = null;
         [Tooltip("星の半径"), SerializeField]
         float starRadius = 4.5f;
-        [Tooltip("海演算に使う各ステージの島のメッシュを持ったオブジェクト"), SerializeField]
-        GameObject[] meshIslands = new GameObject[GameParams.StageMax];
+        [Tooltip("汚れ島のオブジェクト"), SerializeField]
+        GameObject[] dirtyIslands = new GameObject[GameParams.StageMax];
         [Tooltip("星のマテリアル"), SerializeField]
         Material starMaterial = null;
+        [Tooltip("汚れ島のマテリアル"), SerializeField]
+        Material dirtyIslandMaterial = null;
+        [Tooltip("奇麗島のマテリアル"), SerializeField]
+        Material cleanIslandMaterial = null;
         [Tooltip("汚れ海テクスチャ"), SerializeField]
         Texture2D dirtySeaTexture = null;
         [Tooltip("緑化海テクスチャ"), SerializeField]
@@ -37,6 +41,11 @@ namespace GreeningEx2019
         [Tooltip("デバッグ用のSphere"), SerializeField]
         GameObject debugSphere = null;
 #endif
+
+        /// <summary>
+        /// 島の上昇段階
+        /// </summary>
+        public float islandUp = 0f;
 
         /// <summary>
         /// 海用テクスチャーの大きさ
@@ -161,32 +170,12 @@ namespace GreeningEx2019
             seaTexture = new Texture2D(SeaTextureSize, SeaTextureSize, dirtySeaTexture.format, false);
             seaColors = seaTexture.GetPixels32();
 
-            TextAsset asset = Resources.Load(SeaTextureRatesFileName+"1") as TextAsset;
+            TextAsset asset = Resources.Load(SeaTextureRatesFileName+"0") as TextAsset;
             Stream s = new MemoryStream(asset.bytes);
             BinaryReader br = new BinaryReader(s);
             byte[] rates = Decompress( br.ReadBytes((int)s.Length));
 
             Log($"---- readed {rates.Length}");
-
-            /*
-            // GetUVのチェック
-            for (int i=1;i<2;i++)
-            {
-                for (int j=6;j<7;j++)
-                {
-                    Vector3 dir = Quaternion.Euler(360f * (float)(j-6) / 12.0f, 360f * (float)i / 12.0f, 0) * Vector3.forward;
-                    Instantiate(debugSphere, dir * debugSphereRadius, Quaternion.identity, transform);
-                    Vector2 uv = GetUV(dir);
-                    int x = (int)((float)SeaTextureSize * uv.x);
-                    x = Mathf.Min(x, SeaTextureSize - 1);
-                    int y = (int)((float)SeaTextureSize * uv.y);
-                    y = Mathf.Min(y, SeaTextureSize - 1);
-                    Log($"  {x}, {y} = ({y*SeaTextureSize+x} / {seaColors.Length})");
-                    seaColors[y * SeaTextureSize + x] = new Color32((byte)(255*j/8),(byte)(255*(8-j)/8),0,255);
-                    Log($"[{i}, {j}] {dir.x}, {dir.y}, {dir.z} / uv={uv.x}, {uv.y}");
-                }
-            }
-            */
 
             dirtyColors = dirtySeaTexture.GetPixels32();
             cleanColors = cleanSeaTexture.GetPixels32();
