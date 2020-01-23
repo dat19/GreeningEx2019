38f2cb382caf6357ab1a5d51fa58c83d5b8d4e1e
Yu Tanaka <YRK00337@nifty.com>
2020-01-23 23:57:26 +0900

    チュートリアルとナレーションきっかけの作成

---
+++ Assets/Scripts/Message/EventEmitter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GreeningEx2019
+{
+    public delegate bool EventChecker();
+
+    /// <summary>
+    /// マップに配置して、プレイヤーが触れたら呼び出すUnityActionを設定するためのクラスです。
+    /// </summary>
+    public class EventEmitter : MonoBehaviour
+    {
+        [Tooltip("鳴らす音"), SerializeField]
+        SoundController.SeType se = SoundController.SeType.None;
+        [Tooltip("表示するメッセージ。複数設定すると、ランダムでどれか一つを表示します。"), SerializeField]
+        MessageManager.MessageType []messages = new MessageManager.MessageType[0];
+        [Tooltip("発動チェックをするイベント。未設定なら無条件で発動します。"), SerializeField]
+        EventChecker canEmit = null;
+        [Tooltip("発動させたいイベントがあったら登録します。"), SerializeField]
+        UnityEngine.Events.UnityEvent events;
+
+        /// <summary>
+        /// 多重呼び出しを避けるためのフラグ
+        /// </summary>
+        bool isEmitted = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player") && !isEmitted)
+            {
+                if (canEmit != null && !canEmit())
+                {
+                    return;
+                }
+
+                isEmitted = true;
+
+                events.Invoke();
+                if (se != SoundController.SeType.None)
+                {
+                    SoundController.Play(se);
+                }
+                if (messages.Length > 0)
+                {
+                    int index = Random.Range(0, messages.Length);
+                    MessageManager.instance.SetMessage(messages[index]);
+                }
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/Scripts/MessageManager.cs
+++ Assets/Scripts/MessageManager.cs
@@ -17,14 +17,43 @@ namespace GreeningEx2019
         float messageMinTime = 2f;
         [Tooltip("メッセージの最大表示秒数"), SerializeField]
         float messageMaxTime = 5f;
+        [Tooltip("メッセージデータ"), SerializeField]
+        TextAsset messageAsset = null;
+
+        public enum MessageType
+        {
+            None = -1,
+            // Stage1
+            WaterKey,
+            MoveKey,
+            LetsWater,
+            CantSwim,
+            Ivy,
+            ActionKey,
+            LetsMove,
+            LetsGreen,
+            Stage1Nice,
+        }
+
+        /// <summary>
+        /// メッセージの最大表示秒数
+        /// </summary>
+        public static float MessageMaxTime
+        {
+            get
+            {
+                return instance.messageMaxTime;
+            }
+        }
 
         /// <summary>
         /// メッセージを表示した時のTime.time
         /// </summary>
         static float messageStartTime = 0;
 
-        static List<string> messageList = new List<string>();
+        static List<MessageType> messageList = new List<MessageType>();
         static bool messageBoxShowed = false;
+        static string[] messages = null;
 
         private void Awake()
         {
@@ -32,6 +61,16 @@ namespace GreeningEx2019
             messageList.Clear();
             messageBoxShowed = false;
             messageAnim.SetBool("Show", false);
+            string[] lines = messageAsset.text.Split(new char[] { '\n' });
+            messages = new string[lines.Length];
+            for (int i=0;i<lines.Length;i++)
+            {
+                string[] cols = lines[i].Split(new char[] { ',' });
+                if (cols.Length > 1)
+                {
+                    messages[i] = cols[1];
+                }
+            }
         }
 
         private void FixedUpdate()
@@ -71,7 +110,7 @@ namespace GreeningEx2019
         void NextText()
         {
             messageStartTime = 0;
-            messageText.text = messageList[0];
+            messageText.text = messages[(int)messageList[0]];
             messageList.RemoveAt(0);
         }
 
@@ -79,9 +118,12 @@ namespace GreeningEx2019
         /// 指定の文字列をメッセージ表示キューに登録します。
         /// </summary>
         /// <param name="mes"></param>
-        public void SetMessage(string mes)
+        public void SetMessage(MessageType mes)
         {
-            messageList.Add(mes);
+            if (mes != MessageType.None)
+            {
+                messageList.Add(mes);
+            }
         }
     }
 }
